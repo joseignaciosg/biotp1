e6e3fe8f89e077c3da6159d745fbb36320d8a15c
Chino <joseignaciosg9@gmail.com>
2014-11-24 17:48:57 -0300

    ej1 and ej2 ready

--- scripts/ej2.pl
+++ scripts/ej2.pl
@@ -4,21 +4,28 @@ use warnings;
 use Bio::Perl;
 use Bio::SeqIO;
 
+
 # create sequence object from the fasta file
-my $s = Bio::SeqIO->new(
-       -file => $ARGV[0],
-       -format => "fasta"
-);
-
-while (my $st = $s->next_seq)
+my @s;
+my $i=0;
+while ($i < @ARGV )
 {
-       # print $st->seq;
 
-       # against NCBI blast server (remote)
-       my $blast_result = blast_sequence($st->seq);
+ $s[$i] = Bio::SeqIO->new(
+  -file => $ARGV[$i],
+  -format => "fasta"
+ );
 
-       # write blast results to the specified file
-       write_blast(">$ARGV[1]",$blast_result);
+ while (my $st = $s[$i]->next_seq)
+ {
+        # print $st->seq;
+ 
+        # against NCBI blast server (remote)
+        my $blast_result = blast_sequence($st->seq);
+
+        # write blast results to the specified file
+        write_blast(">output/blastout$i",$blast_result);
+ }
+ $i++;
 }
-
 
