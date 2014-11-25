64a5758663235e7c6e03d0c8c155fb435dd69d13
Chino <joseignaciosg9@gmail.com>
2014-11-25 11:22:51 -0300

    ej4

--- scripts/ej4.pl
+++ scripts/ej4.pl
@@ -1,10 +1,4 @@
 #!/usr/bin/perl -w
 use strict;
-use Bio::SeqIO;
-
-my $in = Bio::SeqIO->new(-file => "$ARGV[0]", '-format' => 'genBank');
-my $out = Bio::SeqIO->new(-file => ">$ARGV[1]", '-format' => 'Fasta');
-
-while(my $seq = $in->next_seq() ){
-        $out->write_seq($seq);
-}
+use Bio::Factory::EMBOSS;
+my $f = Bio::Factory::EMBOSS -> new();
