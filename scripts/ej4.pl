89c728ef8824a312308c03145da4d619384e831c
Chino <joseignaciosg9@gmail.com>
2014-11-26 00:10:17 -0300

    ej4

--- scripts/ej4.pl
+++ scripts/ej4.pl
@@ -1,11 +1,28 @@
 #!/usr/bin/perl -w
 
-system "getorf -minsize 300 -sequence " . $ARGV[0] . " -outseq output/result.orf";
-
-
+#system "getorf -minsize 300 -sequence " . $ARGV[0] . " -outseq output/result.orf";
 #seteando la base de datos de prosite
-system "sudo prosextract -prositedir /home/jose/biotp1/scripts/prositedir";
+#system "sudo prosextract -prositedir /home/jose/biotp1/scripts/prositedir";
+#system "patmatmotifs -sequence output/result.orf -outfile output/report.patmatmotifs"
+use strict;
+use Bio::SeqIO;
+use Bio::Factory::EMBOSS;
 
+#primero paso el archivo a fasta 
+my $in = Bio::SeqIO->new(-file => "$ARGV[0]", '-format' => 'genBank');
+my $fastafilename = "$ARGV[0]" .".fas";
+my $out = Bio::SeqIO->new(-file => ">$fastafilename", '-format' => 'Fasta');
 
+while(my $seq = $in->next_seq() ){
+        $out->write_seq($seq);
+}
 
-system "patmatmotifs -sequence output/result.orf -outfile output/report.patmatmotifs"
+my $f = Bio::Factory::EMBOSS -> new();
+
+# get an EMBOSS application  object from the factory
+my $getorf = $f->program('getorf');
+
+my $getorfoutfile = 'output/resultbio1.orf';
+$getorf->run({-sequence => $fastafilename,
+               -outseq   => $getorfoutfile});
+
