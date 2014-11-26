fcf62f31e566424f595124d14740774ab9f6694d
Chino <joseignaciosg9@gmail.com>
2014-11-26 00:40:11 -0300

    ej4

--- scripts/ej4.pl
+++ scripts/ej4.pl
@@ -13,6 +13,9 @@ my $in = Bio::SeqIO->new(-file => "$ARGV[0]", '-format' => 'genBank');
 my $fastafilename = "$ARGV[0]" .".fas";
 my $out = Bio::SeqIO->new(-file => ">$fastafilename", '-format' => 'Fasta');
 
+
+#checktrans
+
 while(my $seq = $in->next_seq() ){
         $out->write_seq($seq);
 }
@@ -21,8 +24,27 @@ my $f = Bio::Factory::EMBOSS -> new();
 
 # get an EMBOSS application  object from the factory
 my $getorf = $f->program('getorf');
+#my $checktrans = $f->program('checktrans');
 
 my $getorfoutfile = 'output/resultbio1.orf';
 $getorf->run({-sequence => $fastafilename,
                -outseq   => $getorfoutfile});
+
+#my $checktransoutgile = 'output/resultbio2.orf';
+#my $checktransoutgileseq = 'output/resultbio2seq.orf';
+#$checktrans->run({-sequence => $fastafilename,
+#               -outfile   => $checktransoutgile,
+#	       -outseq  => $checktransoutgileseq});
+
+#buscando motifs en PROSITE
+my $patmatmotifs = $f->program('patmatmotifs');
+
+my $outfile = '>>output/report.patmatmotifs';
+
+$patmatmotifs->run({-sequence => $getorfoutfile,
+                     -outfile   => $outfile });
+
+
+
+
 
